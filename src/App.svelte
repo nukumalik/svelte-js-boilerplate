a8c8412f265f336536b7bcdfb8750ecd37c07ec7
nuku <nuku@softwareseni.com>
2020-09-10 11:46:54 +0700

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import router from 'page';
+
+  // Components
+  import Navbar from 'components/Navbar';
+
+  // Pages
+  import Home from './pages/Home';
+  import About from './pages/About';
+  import NotFound from './pages/404';
+
+  // Global Variables
+  let page;
+
+  // Routes
+  router('/', () => (page = Home));
+  router('/about', () => (page = About));
+  router('*', () => (page = NotFound));
+  router.start();
+</script>
+
+<Navbar />
+<svelte:component this={page} />
---
+++ src/components/Navbar/index.svelte
@@ -0,0 +1,8 @@
+<style lang="scss" src="./style.scss">
+</style>
+
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/qwerty">Try Dummy Link</a></li>
+</ul>
